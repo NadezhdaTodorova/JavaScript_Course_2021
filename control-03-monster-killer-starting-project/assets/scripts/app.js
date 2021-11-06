ef3ecb438d65f1fee7952c85a9074753dce9b8da
Nadezhda Todorova <nadezhdatodorova55@gmail.com>
2021-11-06 09:37:34 +0200

    Update app.js

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -33,18 +33,17 @@ function writeToLog(event, value, monsterHealt, playerHealth) {
     finalMonsterHealth: monsterHealt,
     finalPlayerhealth: playerHealth,
   };
-  if (
-    event === LOG_EVENT_PLAYER_ATTACK ||
-    event === LOG_EVENT_PLAYER_STRONG_ATTACK
-  ) {
-    logEntry.target = "MONSTER";
-  } else if (
-    event === LOG_EVENT_PLAYER_HEAL ||
-    event === LOG_EVENT_MONSTER_ATTACK
-  ) {
-    logEntry.target = "PLAYER";
-  }
+  switch(event){
+    case LOG_EVENT_PLAYER_ATTACK:
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = "PLAYER";
+      break;
 
+  }
   battleLog.push(logEntry);
 }
 

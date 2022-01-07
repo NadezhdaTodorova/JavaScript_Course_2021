03e2291e28943b9da782795da352608fc97535a1
Nadezhda Todorova <nadezhdatodorova55@gmail.com>
2022-01-07 21:50:32 +0200

    added new project for practise

--- practice-oop-01-starting-setup/assets/scripts/analytics.js
+++
@@ -1,7 +0,0 @@
-const intervalId = setInterval(() => {
-    console.log('starting analytics...');
-}, 2000)
-
-document.getElementById('clearTimeoutBtn').addEventListener('click', ()=>{
-    clearInterval(intervalId);
-});
